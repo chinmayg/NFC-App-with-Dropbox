cf65aa784a71dfc33c4dfe66af19ce48c5f39a04
Arjun Passi <arjun@arjun-LIFEBOOK-T731.(none)>
2013-11-28 20:20:51 -0500

    NFC write string added.

--- NFC/src/com/example/nfc/WelcomeActivity.java
+++ NFC/src/com/example/nfc/WelcomeActivity.java
@@ -91,17 +91,11 @@ public class WelcomeActivity extends TabActivity {
 	        
 	        String subString = nfcData.substring(0, first);
 	        
-	        //Tag contains data for encryption activity
-	        if(subString.contains("EA")){
-	        	intent = new Intent(this,EncryptActivity.class);
+	        //Tag contains data for decryption activity
+	        if(subString.contains("DA")){
+	        	intent = new Intent(this,DecryptActivity.class);
 	    		intent.putExtra("nfcData", nfcData);
 	    		WelcomeActivity.this.startActivity(intent);
-	        }
-	        //Tag contains data for decrypt activity
-	        else if(subString.contains("DA")){
-	        //	intent = new Intent(this,EncryptActivity.class);
-	    	//	intent.putExtra("nfcData", nfcData);
-	    	//	WelcomeActivity.this.startActivity(intent);
 	        }
 	        //Tag contains data for create url activity
 	        else if(subString.contains("CUA")){
@@ -207,7 +201,7 @@ public class WelcomeActivity extends TabActivity {
 		b.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v) {
-				writeTag(WelcomeActivity.this,detectedTag, nfcWriteData);
+				writeTag(WelcomeActivity.this,detectedTag, "EA:");
 			}
 			
 		});
@@ -253,6 +247,7 @@ public class WelcomeActivity extends TabActivity {
 		spec = mTabHost.newTabSpec("changeSetting").setIndicator("Change Setting")
 				.setContent(intent);
 		mTabHost.addTab(spec);
+		
 	}
 	
 	/**
